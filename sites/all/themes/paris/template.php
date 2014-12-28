22ed415b829e00d924f40716d37d446ba41c9c9f
Mike Keran <info@mikekeran.com>
2014-12-28 20:49:16 +0100

    Better teaser truncation.

--- sites/all/themes/paris/template.php
+++ sites/all/themes/paris/template.php
@@ -1,4 +1,27 @@
 <?php
+function paris_preprocess_node(&$variables) {
+  // Check if this is a truncated version of the body or if the teaser is
+  // displaying the entire entry.
+  if ($variables['teaser']) {
+    if (strlen(trim(strip_tags($variables['content']['body'][0]['#markup']))) >= strlen(trim(strip_tags($variables['body'][0]['value'])))) {
+      // Teaser is showing the entire post, so remove the Read more link.
+      if (!empty($variables['content']['links']['node']['#links']['node-readmore'])) {
+        unset($variables['content']['links']['node']['#links']['node-readmore']);
+      }
+    }
+    else {
+      // Otherwise, emphasize the fact that there's more to read with ellipses.
+      // Tuck it in just before the closing </p> tag, assuming that if the final
+      // tag is something else, we don't want the ellipses there.
+      $variables['content']['body'][0]['#markup'] = preg_replace(
+        '/<\/p>$/',
+        '...</p>',
+        $variables['content']['body'][0]['#markup']
+      );
+    }
+  }
+}
+
 function paris_preprocess_rate_template_thumbs_up(&$variables) {
   // Rewrite voting widget based on the user's current vote and the current vote
   // count. Default text goes through if there are no votes recorded.
